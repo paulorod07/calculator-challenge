d65ae4294c33f6d3dddaa817f3122a81f49e1009
Paulo Rodrigues <pauloadesr@hotmail.com>
2020-04-25 21:13:08 -0300

    final project

--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -14,6 +14,7 @@ export class CalculatorComponent implements OnInit {
     grade1: null,
     grade2: null,
     grade3: null,
+    account: null,
   };
 
   constructor(private router: Router, private resultService: ResultService) {}
@@ -21,16 +22,15 @@ export class CalculatorComponent implements OnInit {
   ngOnInit() {}
 
   calcGrade(): void {
-    let grade1 = this.grade.grade1 * 0.25;
-    let grade2 = this.grade.grade2 * 0.25;
-    let grade3 = this.grade.grade3 * 0.5;
+    this.grade.account =
+      this.grade.grade1 * 0.25 +
+      this.grade.grade2 * 0.25 +
+      this.grade.grade3 * 0.5;
 
-    let account = grade1 + grade2 + grade3;
-
-    if (account < 6.2) {
+    if (this.grade.account < 6.2) {
       this.router.navigate(['/result/reprove']);
     }
-    if (account >= 6.2) {
+    if (this.grade.account >= 6.2) {
       this.router.navigate(['/result/approve']);
     }
 
--- src/app/components/calculator/calculator.model.ts
+++ src/app/components/calculator/calculator.model.ts
@@ -3,4 +3,5 @@ export interface Grade {
   grade1: number;
   grade2: number;
   grade3: number;
+  account: number;
 }
